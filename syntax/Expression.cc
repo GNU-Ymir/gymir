dbe30078e044075fe4d86265653623d3af946595
CADOREL Emile <ecadorel@gmail.com>
2021-03-24 21:57:24 +0100

    update

--- syntax/Expression.cc
+++ syntax/Expression.cc
@@ -5,48 +5,48 @@
 namespace syntax {
     
     IExpression::IExpression (const lexing::Word & location) :
-	_location (location)
+        _location (location)
     {}
     
     Expression::Expression (IExpression * expr) : RefProxy<IExpression, Expression> (expr)
     {}
 
     Expression Expression::empty () {
-	return Expression {nullptr};
+        return Expression {nullptr};
     }
 
     bool Expression::isEmpty () const {
-	return this-> _value == nullptr;
+        return this-> _value == nullptr;
     }
     
     Declaration Expression::toDeclaration (const Expression & value, const std::string & comments) {
-	return ExpressionWrapper::init (value.getLocation (), comments, value);
+        return ExpressionWrapper::init (value.getLocation (), comments, value);
     }
     
     void Expression::treePrint (Ymir::OutBuffer & stream, int i)  const {	
-	if (this-> _value == nullptr) {
-	    stream.writef ("%*", i, '\t');
-	    stream.writeln ("<null>");
-	} else this-> _value-> treePrint (stream, i);
+        if (this-> _value == nullptr) {
+            stream.writef ("%*", i, '\t');
+            stream.writeln ("<null>");
+        } else this-> _value-> treePrint (stream, i);
     }
 
     std::string Expression::prettyString () const {
-	if (this-> _value == nullptr) return "";
-	else return this-> _value-> prettyString ();
+        if (this-> _value == nullptr) return "";
+        else return this-> _value-> prettyString ();
     }
 
     const lexing::Word & IExpression::getLocation () const {
-	return this-> _location;
+        return this-> _location;
     }    
 
     const lexing::Word & Expression::getLocation () const {
-	if (this-> _value == nullptr)
+        if (this-> _value == nullptr)
 	    Ymir::Error::halt (Ymir::ExternalError::get (Ymir::NULL_PTR));
-	return this-> _value-> getLocation ();
+        return this-> _value-> getLocation ();
     }    
     
     void IExpression::treePrint (Ymir::OutBuffer & stream, int i) const {
-	stream.writefln ("%*<TODO>", i, '\t');
+        stream.writefln ("%*<TODO>", i, '\t');
     }
 
     // std::string IExpression::prettyString () const {
@@ -54,7 +54,5 @@ namespace syntax {
     // }
     
     IExpression::~IExpression () {}
-
-
     
 }
--- ymir1.cc
+++ ymir1.cc
@@ -44,9 +44,9 @@ struct GTY (()) lang_identifier
 /* The resulting tree type.  */
  
 union GTY ((desc ("TREE_CODE (&%h.generic) == IDENTIFIER_NODE"),
-	    chain_next ("CODE_CONTAINS_STRUCT (TREE_CODE (&%h.generic), "
-			"TS_COMMON) ? ((union lang_tree_node *) TREE_CHAIN "
-			"(&%h.generic)) : NULL"))) lang_tree_node
+            chain_next ("CODE_CONTAINS_STRUCT (TREE_CODE (&%h.generic), "
+                        "TS_COMMON) ? ((union lang_tree_node *) TREE_CHAIN "
+                        "(&%h.generic)) : NULL"))) lang_tree_node
 {
   union tree_node GTY ((tag ("0"), desc ("tree_node_structure (&%h)"))) generic;
   struct lang_identifier GTY ((tag ("1"))) identifier;
@@ -57,7 +57,7 @@ union GTY ((desc ("TREE_CODE (&%h.generic) == IDENTIFIER_NODE"),
 struct GTY (()) language_function
 {
 
-    int dummy;
+  int dummy;
 };
  
 /* Language hooks.  */
@@ -66,7 +66,7 @@ static bool
 ymir_langhook_init (void)
 {
   /* NOTE: Newer versions of GCC use only:
-           build_common_tree_nodes (false);
+     build_common_tree_nodes (false);
      See Eugene's comment in the comments section. */
   build_common_tree_nodes (false);
  
@@ -82,21 +82,21 @@ ymir_langhook_init (void)
 static void
 ymir_init_options (unsigned int argc ATTRIBUTE_UNUSED, cl_decoded_option * decoded_options ATTRIBUTE_UNUSED)
 {
-    global::State::instance ().setExecutable (decoded_options [0].arg);
-    for (unsigned int i = 0 ; i < argc ; i++) {
-	//const char * arg = decoded_options [i].arg;
-	switch (decoded_options [i].opt_index) {
-	case OPT_g :
-	case OPT_ggdb :
+  global::State::instance ().setExecutable (decoded_options [0].arg);
+  for (unsigned int i = 0 ; i < argc ; i++) {
+    //const char * arg = decoded_options [i].arg;
+    switch (decoded_options [i].opt_index) {
+    case OPT_g :
+    case OPT_ggdb :
 	    global::State::instance ().activateDebug (true);
 	    global::State::instance ().activateVersion (global::CoreNames::get (global::DEBUG_VERSION));
 	    break;
-	case OPT_v : global::State::instance ().activateVerbose (true); break;
-	case OPT_nostdinc : global::State::instance ().activateStandalone (true); break;
-	}
+    case OPT_v : global::State::instance ().activateVerbose (true); break;
+    case OPT_nostdinc : global::State::instance ().activateStandalone (true); break;
     }
+  }
     
-    // Options OPT_l and cie...
+  // Options OPT_l and cie...
 }
 
 static void
@@ -121,45 +121,47 @@ ymir_init_options_struct (gcc_options *opts) {
 
 static unsigned int
 ymir_option_lang_mask (void) {
-    return CL_YMIR;
+  return CL_YMIR;
 }
     
 static bool
 ymir_langhook_handle_option (size_t scode, const char *arg, long int value ATTRIBUTE_UNUSED,
-		   int kind ATTRIBUTE_UNUSED, location_t loc ATTRIBUTE_UNUSED,
-		   const struct cl_option_handlers *handlers ATTRIBUTE_UNUSED)
+                             int kind ATTRIBUTE_UNUSED, location_t loc ATTRIBUTE_UNUSED,
+                             const struct cl_option_handlers *handlers ATTRIBUTE_UNUSED)
 {
-    opt_code code = (opt_code) scode;
-    if (code == OPT_I) {
-	// Add include dir
-	global::State::instance ().addIncludeDir (arg);
+  opt_code code = (opt_code) scode;
+  if (code == OPT_I) {
+    // Add include dir
+    global::State::instance ().addIncludeDir (arg);
 	
-    } else if (code == OPT_iprefix) {
-	global::State::instance ().setPrefix (arg);
-    } else if (code == OPT_v) {
-	return false;
-	// set verbose 
-    } else if (code == OPT_nostdinc)  {
-	// no std include
-	global::State::instance ().activateStandalone (true);
-    } else if (code == OPT_fdoc) {
-	// dump documentation
-	global::State::instance ().activateDocDumping (true);
-    } else if (code == OPT_fversion_) {
-	global::State::instance ().activateVersion (arg);
-    } else if (code == OPT_imultilib) {
-	// set multilib
-    } else {
-	return false;
-    }
+  } else if (code == OPT_iprefix) {
+    global::State::instance ().setPrefix (arg);
+  } else if (code == OPT_v) {
+    return false;
+    // set verbose
+  } else if (code == OPT_nostdinc)  {
+    // no std include
+    global::State::instance ().activateStandalone (true);
+  } else if (code == OPT_fdoc) {
+    // dump documentation
+    global::State::instance ().activateDocDumping (true);
+  } else if (code == OPT_fversion_) {
+    global::State::instance ().activateVersion (arg);
+  } else if (code == OPT_imultilib) {
+    // set multilib
+  } else {
+    return false;
+  }
     
-    return true;
+  return true;
 }
 
 static void
 ymir_langhook_parse_file (void)
 {
-    ymir_parse_files (num_in_fnames, in_fnames);
+  auto t = clock () ;
+  ymir_parse_files (num_in_fnames, in_fnames);
+  println ("All : ", ((float) (clock () - t))/CLOCKS_PER_SEC);
 }
  
 static tree
@@ -167,43 +169,43 @@ ymir_langhook_type_for_mode (enum machine_mode mode, int unsignedp)
 {
   
   if (mode == TYPE_MODE (float_type_node))
-      return float_type_node;
+  return float_type_node;
   
   if (mode == TYPE_MODE (double_type_node))
-      return double_type_node;
+  return double_type_node;
  
   if (mode == TYPE_MODE (intQI_type_node))
-    return unsignedp ? unsigned_intQI_type_node : intQI_type_node;
+  return unsignedp ? unsigned_intQI_type_node : intQI_type_node;
   if (mode == TYPE_MODE (intHI_type_node))
-    return unsignedp ? unsigned_intHI_type_node : intHI_type_node;
+  return unsignedp ? unsigned_intHI_type_node : intHI_type_node;
   if (mode == TYPE_MODE (intSI_type_node))
-    return unsignedp ? unsigned_intSI_type_node : intSI_type_node;
+  return unsignedp ? unsigned_intSI_type_node : intSI_type_node;
   if (mode == TYPE_MODE (intDI_type_node))
-    return unsignedp ? unsigned_intDI_type_node : intDI_type_node;
+  return unsignedp ? unsigned_intDI_type_node : intDI_type_node;
   if (mode == TYPE_MODE (intTI_type_node))
-    return unsignedp ? unsigned_intTI_type_node : intTI_type_node;
+  return unsignedp ? unsigned_intTI_type_node : intTI_type_node;
  
   if (mode == TYPE_MODE (integer_type_node))
-    return unsignedp ? unsigned_type_node : integer_type_node;
+  return unsignedp ? unsigned_type_node : integer_type_node;
  
   if (mode == TYPE_MODE (long_integer_type_node))
-    return unsignedp ? long_unsigned_type_node : long_integer_type_node;
+  return unsignedp ? long_unsigned_type_node : long_integer_type_node;
  
   if (mode == TYPE_MODE (long_long_integer_type_node))
-    return unsignedp ? long_long_unsigned_type_node
-		     : long_long_integer_type_node;
+  return unsignedp ? long_long_unsigned_type_node
+    : long_long_integer_type_node;
  
   if (COMPLEX_MODE_P (mode))
-    {
-      if (mode == TYPE_MODE (complex_float_type_node))
-	return complex_float_type_node;
-      if (mode == TYPE_MODE (complex_double_type_node))
-	return complex_double_type_node;
-      if (mode == TYPE_MODE (complex_long_double_type_node))
-	return complex_long_double_type_node;
-      if (mode == TYPE_MODE (complex_integer_type_node) && !unsignedp)
-	return complex_integer_type_node;
-    }
+  {
+    if (mode == TYPE_MODE (complex_float_type_node))
+    return complex_float_type_node;
+    if (mode == TYPE_MODE (complex_double_type_node))
+    return complex_double_type_node;
+    if (mode == TYPE_MODE (complex_long_double_type_node))
+    return complex_long_double_type_node;
+    if (mode == TYPE_MODE (complex_integer_type_node) && !unsignedp)
+    return complex_integer_type_node;
+  }
  
   /* gcc_unreachable */
   return NULL;
@@ -211,21 +213,21 @@ ymir_langhook_type_for_mode (enum machine_mode mode, int unsignedp)
  
 static tree
 ymir_langhook_type_for_size (unsigned int bits,
-			     int unsignedp)
+                             int unsignedp)
 {
-    if (bits <= TYPE_PRECISION (byte_type_node))
+  if (bits <= TYPE_PRECISION (byte_type_node))
 	return unsignedp ? ubyte_type_node : byte_type_node;
 
-    if (bits <= TYPE_PRECISION (short_type_node))
+  if (bits <= TYPE_PRECISION (short_type_node))
 	return unsignedp ? ushort_type_node : short_type_node;
 
-    if (bits <= TYPE_PRECISION (int_type_node))
+  if (bits <= TYPE_PRECISION (int_type_node))
 	return unsignedp ? uint_type_node : int_type_node;
     
-    if (bits <= TYPE_PRECISION (long_type_node))
+  if (bits <= TYPE_PRECISION (long_type_node))
 	return unsignedp ? ulong_type_node : long_type_node;
     
-    return NULL;
+  return NULL;
 }
  
 /* Record a builtin function.  We just ignore builtin functions.  */
@@ -239,8 +241,8 @@ ymir_langhook_builtin_function (tree decl)
 static bool
 ymir_langhook_global_bindings_p (void)
 {
-    gcc_unreachable ();
-    return true;
+  gcc_unreachable ();
+  return true;
 }
  
 static tree
