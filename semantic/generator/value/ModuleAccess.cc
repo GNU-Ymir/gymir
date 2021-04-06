99f44a7e99bf608634dfe4d769ec65bce0d7e7c0
CADOREL Emile <ecadorel@gmail.com>
2021-04-06 16:53:36 +0200

    syntax of module access for error

--- semantic/generator/value/ModuleAccess.cc
+++ semantic/generator/value/ModuleAccess.cc
@@ -43,10 +43,7 @@ namespace semantic {
 	}
 
 	std::string ModuleAccess::prettyString () const {
-	    if ((Symbol {this-> _ref}).is<ModRef> ()) {		
-		return Ymir::format ("ref %", (Symbol {this-> _ref}).getRealName ());
-	    } 
-	    return Ymir::format ("%", (Symbol {this-> _ref}).getRealName ());
+	    return Ymir::format ("mod %", (Symbol {this-> _ref}).getRealName ());
 	}
 	
     }
